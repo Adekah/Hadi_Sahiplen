56c092373a5cef0519fc9dafa5cf2a8d14daeea9
Adekah <adekahuseyin@hotmail.com>
2018-10-23 13:28:15 +0300

    son hali

--- HadiSahiplen/Scripts/Side.js
+++ HadiSahiplen/Scripts/Side.js
@@ -12,3 +12,21 @@ $(document).on("click", "#signup_ID", function () {
     event.preventDefault();
     return false;
 })
+
+
+
+
+$(document).on("click", "#btnSignUp_ID", function () {
+
+    var username = $('#İnput_Name_ID').val();
+    //alert(username);
+
+    $("#modal-info_Validate").modal({ show: true, keyboard: true, backdrop: 'static' });
+
+    $(".modal-body").empty();
+
+    $(".modal-body").append("<i class='fa fa-chevron-right'></i><span style='padding-left:10px'>Aşağıdaki Ürünler Tablosundan Ürün Seçiniz</span>");
+
+    event.preventDefault();
+    return false;
+})
